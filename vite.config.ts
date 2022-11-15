fc8ebfbee7741c8b8159192a728bc4732bff2725
talves <tony@3alves.com>
2022-11-14 16:35:22 -0800

    branch: netlify edge

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     solid({
       extensions: [".mdx", ".md", ".tsx"],
-      adapter: netlify({ edge: false })
+      adapter: netlify({ edge: true })
     }),
   ],
 });
